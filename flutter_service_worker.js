1c3adfc89020786626aed07bc9b251ad379d4b08
Yash Patel <yrp.baps@gmail.com>
2023-02-09 16:34:06 +0530

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "029e27779ecd92dfc57d6f31c3de9022",
-"/": "029e27779ecd92dfc57d6f31c3de9022",
-"main.dart.js": "1cab8455b06deba6aa1bbedbb3fedaef",
+"index.html": "338914e4e947b71039f17136144c5ac1",
+"/": "338914e4e947b71039f17136144c5ac1",
+"main.dart.js": "57f9471384b033a35e7fedc7f72a3602",
 "manifest.json": "7b48859b17d6f9351202e2757b340fed",
 "version.json": "322dfa94430cc69aa45ce83495f46466"
 };
